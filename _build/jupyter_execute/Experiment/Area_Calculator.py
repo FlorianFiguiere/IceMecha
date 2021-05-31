c73c530e64093b4145671af5c5ad1174d27b88d6
FlorianFiguiere <florian.figuiere@grenoble-inp.org>
2021-05-31 16:33:11 +0200

    V1.0

---
+++ _build/jupyter_execute/Experiment/Area_Calculator.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import cmath
+
+Dlarge=52.2
+Dsmall=42.0
+
+#attribution of variables used during the program.
+
+r=Dlarge/2 #larger radius of the circle.
+h=Dsmall-r #smaller radius of the sample.
+f=Dlarge-Dsmall #deflection
+alpha=2*cmath.acos(h/r).real
+g=9.80665 #m.s^-2
+sigmath=0.8 #MPa
+
+#print(alpha*360/(2*cmath.pi))
+
+
+# In[2]:
+
+
+#Determination of the area of the bot/top surface of the sample. (in mm²)
+Ac=cmath.pi*pow(r,2) #Area of a circle
+As=Ac*alpha/(2*cmath.pi) #Area of the circular sector alpha
+At=1/2*pow(r,2)*cmath.sin(alpha).real #Area of the isosceles triangle in the circle
+A1=As-At #Area of the segment circle
+
+Af=Ac-A1#Final area wanted
+
+#print("Ac=",Ac, "As=",As,"At=",At,"A1=",A1)
+print("Area of the sample = {:.2f} mm²".format(Af))
+
+
+# In[3]:
+
+
+#Determination of the theoretical weight
+
+Mth=(sigmath*Af-22.38)/(0.14)
+M2=(sigmath*Af-19.42)/(0.14)
+print("Theoretical weight= {:.1f} g or {:.3f} kg".format(Mth,Mth*0.001))
+
+
+# In[ ]:
+
+
+
+
---
+++ _build/jupyter_execute/Experiment/Build_granular_ice.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Introduction
+# 
+# In order to do the experiments, we have to prepare good samples which fit the wanted characteristic (granular ice). No need to make some sampling on natural ice (in moutain or elsewhere) because we can create this ice from chemist water (clean water). Be aware that a few days are required getting the wanted ice so proceed with patience and precision in order to avoid loosing time.
+# 
+# ##  Preparation of the water
+# 
+# - Fill up a balloon with chemist water (clean water).
+# - Boil it during 45 minutes approximately.
+# - Cap the bottle.
+# - Let it cool down at room temperature than put it at 0°C in a chamber (at least one night so the water is at the chamber temperature).
+# 
+# ## Preparation of the snow
+# 
+# - Fill up a box at two third with clean water.
+# - Let it freeze in a chamber at low temperature until there is no water left in the box.
+# - Move the box in a room above 5°C, reverse it above a sink and wait for the ice block to fall.
+# - Cut small pieces of ice and crush it into snow. Use a 2 mm sieve to keep only grain bellow 2mm radius.
+# - Fill up a crystallizer up to 4/5. (Slam gently the column with the "carpet" in order to pack the snow and reduce as possible the air)
+# - Fold a tissue and put it inside de crystallizer above the snow. Make sure the tissue blocks most of the snow. 
+# 
+# ## Creation of the granular ice
+# 
+# - Move the crystallizer to a chamber at 0°C, put some silicone grease between the crystallizer and his cover.  The grease should be applied in a continuous line. Put the cover at the top of the crystallizer.
+# - Connect it to the ballon which contain the water prepared above and a vacuum pump. 
+# - Start the pump and let it work during approximately 1 h in order to make the void.
+# - Pinch off the tube and stop the pump.
+# - Reverse the balloon of water and wait for the water to drop in the crystallizer. (The tissue will allow a more homogeneous distribution of water inside the crystallizer).
+# - A line should appeared at the bottom of the crystallizer. When it reach near the edge of the snow at the top, reverse again the balloon.
+# - Allow some air to go in the crystallizer by releasing for a brief instant the pinch. This will pack the snow again, moving the air trapped inside the snow outside.
+# - Use a peltier device at -15°C to create a growth from bottom to top. Put some thermal insulator around the crystallizer. (Control the peltier device's temperature with a thermometer)
+# - When there is no water left in the crystallizer, unmould it like the ice. This should take around 45min-50min. Watch out for any unwanted shock which can brittle the sample. Also don't wait too much : The sample must stay the less possible at room temperature.
+# - Seal it and store it at -5°C for recrystallisation. The seal will stop the sample to be damaged by the sublimation of ice.
+# 
+# Now, we have a sample of granular ice. But we need to machine it : Reduce the radius, the heigh and correct the parallelism of each base of the sample. We will cut a bit of the sample in order to create a thin film but also a surface on the sample which will receive the mouchti required for Digital Image Correlation (DIC).
+
+# In[ ]:
+
+
+
+
