8a243c7cb8c9fb41d9d38061eae2aac4797d3c9d
FlorianFiguiere <florian.figuiere@grenoble-inp.org>
2021-06-01 17:08:41 +0200

    V1.2

---
+++ _build/jupyter_execute/Experiment/Machining_of_the_sample.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Machining of the Sample
+# In this part I will present the different step to follow before obtaining the correct geometry for the sample.
