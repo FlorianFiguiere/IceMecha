9000337a07d540fee9b849f217a7fbb5f42b3692
FlorianFiguiere <florian.figuiere@grenoble-inp.org>
2021-06-03 12:12:52 +0200

    Add yml file and remove some files

--- igeglace/_build/jupyter_execute/Experiment/Machining_of_the_sample.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Machining of the Sample
-# In this part I will present the different step to follow before obtaining the correct geometry for the sample.
