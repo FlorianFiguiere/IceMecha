0da2e952f816d84b8ca940e510292e995f1fc085
FlorianFiguiere <florian.figuiere@grenoble-inp.org>
2021-06-09 14:52:11 +0200

    Reorganize structure and add some improvement to calculate_area

---
+++ _build/jupyter_execute/Docs/Creep_Experiment.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Creep experiment
+# 
+# use Digital Image Correlation (DIC) in order to highlight the dynamic recrystallisation in ice when under a continuous stress (compression). This will allow us to study the behavior of ice in ice cap. 
+# 
+
+# Use [this link](Area_Calculator.ipynb) to obtain the theoretical weight needed for the creep experiment.
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ _build/jupyter_execute/Intro.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Index
+# =======================
+# 
+# This Jupyter Book will talk about protocols and experiment used at IGE. The aim is to list protocols and experiment in one place, making easier researches and documentations about ice mechanics. There is below a table of content :
+# 
+# - [Building Granular Ice](Docs/Build_Granular_Ice.ipynb) : Protocol on how to build granular ice from clean water.
+# - [What's the area of my sample ?](Docs/Area_Calculator.ipynb) : Function who determine the area of the sample with two measure.
+# - [Machining of the sample for creep experiment](Docs/Creep_Experiment.ipynb) : Protocol on how to machining a sample for a creep experiment
+# 
+
+# In[ ]:
+
+
+
+
