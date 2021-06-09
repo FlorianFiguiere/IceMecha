2862db1953fbba446749093d59727686601fdb5b
FlorianFiguiere <florian.figuiere@grenoble-inp.org>
2021-06-09 16:44:34 +0200

    Adding content related to Thin Ice Film

--- _build/jupyter_execute/Docs/Thin_Ice_Film_AITA.py
+++ _build/jupyter_execute/Docs/Thin_Ice_Film_AITA.py
@@ -2,3 +2,18 @@
 # coding: utf-8
 
 # # AITA
+# 
+# 
+# In this part, we will explain how to set and use AITA. If you haven't made a thin ice film yet, you can see how to get one from a sample [here](Thin_Ice_Film_Making.ipynb).
+# 
+# ## Introduction to AITA
+# 
+# ## Setup of AITA
+# 
+# ## Acquisition 
+
+# In[ ]:
+
+
+
+
--- _build/jupyter_execute/Docs/Thin_Ice_Film_Making.py
+++ _build/jupyter_execute/Docs/Thin_Ice_Film_Making.py
@@ -12,9 +12,40 @@
 # 
 # ## Reducing the thickness of the film
 # 
+# ### Create a flat surface.
+# 
+# - Firstly, check if the rasor blade is still functionnal and without default. If needed, change it. You can check [this page](Rasor_Blade_Change.ipynb) if you need help on how to change the blade.
+# - Start the vacuum pump and put the glass panel on top of the red circle (see figure)
+# - Make sure that the glass panel won't move and won't hit borders when sliding the whole block on rail.
+# - The lever figure XX allow you to unlock the height of the support. This height can be set with the other lever figure XX. 
+# - Once the height film is close to the rasor blade, you can start making slow back and forth movement with the support. With every loop, the height of the support will increment with the value set by the indicator in XX. On top of that, it's possible to improve manually and precisely the height of the support with the wheel in XX.
+# - Continue the back and forth movement until the rasor blade cuts the entire area of the sample. At this point, the upper surface is (almost perfectly) flat.
+# - Stop the vacuum pump and move horizontally the glass panel to break the void created bellow. Moving horizontally will avoid abrupt and uncontrolled movement which can break the ice film.
+# 
+# ### Flipping of the ice film.
+# 
+# - Use a cutter to break the two bubble made before.
+# - Place a thinner glasse panel close to the glass panel used and drag the ice film unto the new glass panel.
+# - Flip the film carefuly.
+# - Using a pipette, make a line of clean water between the glass panel and one of the side of the film.
+# - Wait until the water freeze and repeat the previous step for each side. Waiting between each side will avoid creating stress within the thin film.
+# - Finnaly, make a large bubble of water next to the film (close to the bottom). When the bubble is frozen, you can proceed to the next step.
+# 
+# ### Preparation of the other surface
+# 
+# - Similary to the creation of the first flat surface, fix the glass panel unto the support and set his height.
+# - Start again the back and forth movement and continue until the thickness of the ice film is close to 0,35 mm.
+# - To measure the thickness of the film, used a XXX and measure the thickness of the bubble (which is equal to the film) plus support and glass pannel) and withdraw the thickness of the support and glass panel. Don't hesitate to reduce the incrementation made at the end of each loop and slower the pace when coming close to the thickness wanted.
+# 
+# The making of a thin ice film is done. You can move on to [the analysis with AITA](Thin_Ice_Film_AITA.ipynb).
+# 
 
 # In[ ]:
 
 
 
 
+
+# 
+
+# 
